0d738417f0bbee0cb6aa36eb55a84eece3b5ec3c
Hector Sosa <hectorsosa2022@outlook.es>
2022-10-17 22:46:51 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio1_3/Ejercicio1_3/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using Ejercicio1_3.Controller;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+
+namespace Ejercicio1_3
+{
+    public partial class App : Application
+    {
+        static DataBase db;
+
+        public static DataBase DBase
+        {
+            get
+            {
+                if(db == null)
+                {
+                    String FolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Persona.db3");
+                    db = new DataBase(FolderPath);
+                }
+                return db;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Ejercicio1_3/Ejercicio1_3/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Ejercicio1_3.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio1_3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnregistrar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CreateUsers());
+        }
+
+        private async void btnlistar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ListUsers());
+        }
+    }
+}
